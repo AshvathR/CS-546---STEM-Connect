4acf109b2a9e4664aa1fe949f98afab2ce6b2391
shubham warghade <72713320+swarghade@users.noreply.github.com>
2021-05-11 21:23:37 -0400

    Unified route for company and user profile

--- routes/companyView.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-router.get('/', async(req,res)=> {
-    console.log(req.session._id)
-    res.render('company/profile', { title: "Company Details" ,  auth: true, notLoginPage: true});
-})
-
-module.exports = router;
--- routes/profileView.js
+++ routes/profileView.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 const data = require('../data/');
 router.get('/', async(req,res)=> {
-    console.log(req.session._id)
-    const userInfo = await user.getUserID(req.session._id)
-    console.log(userInfo)
-
-    res.render('employee/profile', { title: "Company Details" ,  auth: true, notLoginPage: true});
+    console.log(req.session)
+    if (req.session.currenUser == 'employee') {
+        res.render('employee/profile', { title: "Company Details" ,  auth: true, notLoginPage: true});
+    } else {
+        res.render('company/profile', { title: "Company Details" ,  auth: true, notLoginPage: true});
+    }
 })
 
 module.exports = router;
