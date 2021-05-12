410853099abd650fbb5d66b7bd1914b91034f25f
shubham warghade <72713320+swarghade@users.noreply.github.com>
2021-05-11 20:34:37 -0400

    Update profileView.js

--- routes/profileView.js
+++ routes/profileView.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const data = require('../data/');
 router.get('/', async(req,res)=> {
     console.log(req.session._id)
+    const userInfo = await user.getUserID(req.session._id)
+    console.log(userInfo)
+
     res.render('employee/profile', { title: "Company Details" ,  auth: true, notLoginPage: true});
 })
 
